02000510cd54972f1d9b5a56d8766388eda571ac
shaikdilawar <dillushaik24@gmail.com>
2024-11-05 20:22:37 +0530

    Add files via upload

---
+++ dsa/Calculator.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+//takes input from user till user does not press X or x.
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int ans=0;
+        while(true){
+
+            System.out.println("Enter the operator");
+            char op = sc.next().trim().charAt(0);
+            if(op =='+'|| op =='-' || op =='*' || op =='/' || op =='%' )
+            {
+                System.out.println("Enter two numbers");
+                int num1 = sc.nextInt();
+                int num2 = sc.nextInt();
+
+                if(op=='+') {
+                    ans = num1 + num2;
+                }
+                if(op=='-') {
+                    ans = num1 - num2;
+                }
+                if(op=='*') {
+                    ans = num1 * num2;
+                }
+                if(op=='/') {
+                    if(num2!=0){
+                    ans = num1 / num2;
+                }}
+                if(op=='%') {
+                    ans = num1 % num2;
+                }
+            } else if (op == 'x' || op == 'X') {
+                break;
+            }
+            else
+                System.out.println("Invalid operator");
+            System.out.println(ans);
+        }
+
+    }
+}
